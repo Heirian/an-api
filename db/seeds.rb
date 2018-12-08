0aa31ba47a070549fa640612b6a3e9c00a6406f8
Heirian <shinkcc@gmail.com>
2018-12-08 00:23:55 -0200

    Updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(login: 'jsmith', name: 'John Smith', provider: 'github')
 
-Article.create([
-                 { title: 'Article 1', content: 'Content 1', slug: 'slug-1' },
-                 { title: 'Article 2', content: 'Content 2', slug: 'slug-2' },
-                 { title: 'Article 3', content: 'Content 3', slug: 'slug-3' },
-                 { title: 'Article 4', content: 'Content 4', slug: 'slug-4' }
-               ])
+Article.create(
+  [{ title: 'Article 1', content: 'Content 1', slug: 'slug-1', user: user },
+   { title: 'Article 2', content: 'Content 2', slug: 'slug-2', user: user },
+   { title: 'Article 3', content: 'Content 3', slug: 'slug-3', user: user },
+   { title: 'Article 4', content: 'Content 4', slug: 'slug-4', user: user }]
+)
+
+Comment.create(
+  [{ content: 'Awesome text', article: Article.first, user: user },
+   { content: 'First', article: Article.first, user: user },
+   { content: 'Wow', article: Article.last, user: user }]
+)
